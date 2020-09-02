eae13a9811884f6de8ae3fc9bd17c4d3f1938b5d
Dave Goldstein <dpg3k@virginia.edu>
2020-09-02 11:32:01 -0400

    Bring in packaging assets. Make a layered build. Update to latest Alpine

---
+++ package/scripts/entry.sh
@@ -0,0 +1,8 @@
+# run application
+
+# run from here, since application expects json template in templates/
+cd bin; ./aries-jstor -publicurl $ARIES_ARTSTOR_URL -url $ARIES_JSTOR_URL -project $ARIES_JSTOR_PROJECT -email $ARIES_JSTOR_EMAIL -pass $ARIES_JSTOR_PASS
+
+#
+# end of file
+#
